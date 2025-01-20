9b932e24d9cf0e52edeb94c265ddda63f62214a4
bigri <bigrizon00@gmail.com>
2025-01-20 15:40:37 +0300

    HomeWork

---
+++ src/main/java/org/asd/Lection2/FibonacciSequence.java
@@ -0,0 +1,70 @@
+package org.asd.Lection2;
+
+
+/** Написать класс FibonacciSequence, в котором есть поле length - длинна последовательности Фибоначчи.
+ Написать 3 метода в этом классе, возвращающих последовательность фибоначчи размера length в виде массива. Пример [0, 1, 1, 2, 3, 5] для length = 6.
+ Каждый метод использует свой вариант цикла: 1 метод через цикл for, 2 через while, 3 через do-while.
+ Написать конструктор с параметром и без параметров для этого класса. Не допускать возможность установки отрицательного значения length.
+ Написать программу (отдельный класс с методом main) в котором создать объект класса FibonacciSequence и протестировать работу всех получившихся методов.
+ */
+
+
+public class FibonacciSequence {
+    private int lenth = 0;
+
+    public FibonacciSequence (){
+    }
+
+    public void setLenth(int lenth) {
+        this.lenth = lenth;
+    }
+
+    public  FibonacciSequence (int lenth){
+        this.lenth = lenth;
+    }
+
+    public int [] generateByFor(){
+
+        int[] fibSequence = new int[lenth];
+        fibSequence[0] = 0;
+        fibSequence[1] = 1;
+        int index = 2;
+        for (int i = 2; i < lenth; i++) {
+            fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+            fibSequence[index] = fibSequence[i];
+            index++;
+            if (lenth < 0){
+                return null;
+            }
+        }
+
+        return fibSequence;
+    }
+
+    public int[] generateByWhile(){
+        int[] fibSequence = new int[lenth];
+        fibSequence[0] = 0;
+        fibSequence[1] = 1;
+
+        int i = 2;
+        while (i < lenth ){
+            fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+            i++;
+
+        }
+        return fibSequence;
+    }
+
+    public int[] generateByDoWhile(){
+        int[] fibSequence = new int[lenth];
+        fibSequence[0] = 0;
+        fibSequence[1] = 1;
+        int i = 2;
+        do{
+            fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+            i++;
+
+        } while (i < lenth );
+        return fibSequence;
+    }
+}
---
+++ src/main/java/org/asd/Lection2/Main.java
@@ -0,0 +1,13 @@
+package org.asd.Lection2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        FibonacciSequence fibonacciSequence = new FibonacciSequence();
+        fibonacciSequence.setLenth(20);
+        System.out.println("Последовательность с For :" + Arrays.toString(fibonacciSequence.generateByFor()));
+        System.out.println("Последовательность с While :" + Arrays.toString(fibonacciSequence.generateByWhile()));
+        System.out.println("Последовательность с DoWhile :" +Arrays.toString(fibonacciSequence.generateByDoWhile()));
+    }
+}
